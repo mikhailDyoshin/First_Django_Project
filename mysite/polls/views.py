5283a197ce1e519b9202cf6e665bd2f86cf6196b
Mikhail Dyoshin <mikhail.dyoshin@gmail.com>
2022-10-04 14:22:53 +0300

    Wrote more views
    Added some simple functions to views.py and wired them into the polls.urls with path() calls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse(f'You\'re looking at question {question_id}')
+
+def results(request, question_id):
+    responce = f'You\'re looing at the resulsts of  question {question_id}'
+    return HttpResponse(responce)
+
+def vote (request, question_id):
+    return HttpResponse(f'You\'re voting on question {question_id}')
