f91278c1d47aaf4a80a49f3734faa40ec7988d77
Mikhail Dyoshin <mikhail.dyoshin@gmail.com>
2022-10-04 15:16:33 +0300

    Created a template that index view can use
    Added polls/templates/polls/index.html file, changed index function in polls/veiws.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
+from django.template import loader
 
 def index(request):
     # Take 5 the latest questions in the system
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
 
-    # Form the responce
-    output = '; '.join([q.question_text for q in latest_question_list])
+    # Choose the template that will show the response
+    template = loader.get_template('polls/index.html')
 
-    return HttpResponse(output)
+    # Form a context for the template (form the content for the html page)
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
