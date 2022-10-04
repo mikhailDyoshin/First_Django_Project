b47d97d64aa8e7e37bd514cc74f46813b449bb18
Mikhail Dyoshin <mikhail.dyoshin@gmail.com>
2022-10-04 14:59:32 +0300

    Wrote a view that actually does something
    Edited index function in views.py that shows the last 5 questions in the system

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # Take 5 the latest questions in the system
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    # Form the responce
+    output = '; '.join([q.question_text for q in latest_question_list])
+
+    return HttpResponse(output)
+
 
 def detail(request, question_id):
     return HttpResponse(f'You\'re looking at question {question_id}')
 
+
 def results(request, question_id):
     responce = f'You\'re looing at the resulsts of  question {question_id}'
     return HttpResponse(responce)
+
 
 def vote (request, question_id):
     return HttpResponse(f'You\'re voting on question {question_id}')
