e7ccd0f8006e4d18a9def6a0ccec6dfdb6a5f7b6
Mikhail Dyoshin <mikhail.dyoshin@gmail.com>
2022-10-06 13:42:10 +0300

    Added tests for get_queryset in DetailView class (polls/views.py)
    The tests check whether a response for the page with question which publication date is in the future returns 404 status code, and whether a response for the page with question which publication date is in the past returns the question's text.

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -144,3 +144,37 @@ class QuestionIndexViewTest(TestCase):
             response.context['latest_question_list'],
             [question2, question1]
         )
+
+class QuestionDetailViewTest(TestCase):
+
+    def test_future_question(self):
+        """
+            The detail view of a question
+            which publication date is in the future
+            returns a 404 not found.
+        """
+
+        future_question = create_question("Future question", days=5)
+
+        url = reverse('polls:detail', args=(future_question.id,))
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 404)
+
+
+    def test_past_question(self):
+        """
+            The detail view of a question
+            with the past publication date
+            displays the question's text. 
+        """
+
+        past_question = create_question("Past question", days=-5)
+
+        url = reverse('polls:detail', args=(past_question.id,))
+
+        response = self.client.get(url)
+
+        self.assertContains(response, past_question.question_text)
+        
