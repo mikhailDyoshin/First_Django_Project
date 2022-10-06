54232f0847edda662e95f8805bf0264bb095b798
Mikhail Dyoshin <mikhail.dyoshin@gmail.com>
2022-10-06 10:13:59 +0300

    Updated get_queryset method in IndexView class (polls/views.py)
    Now the get_queryset method returns the last 5 questions ignoring those, which publication date is in the future

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,13 +7,22 @@ from django.http import Http404
 from django.urls import reverse
 from django.db.models import F
 from django.views import generic
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+
+        """
+            Returns the last 5 questions not including
+            those to be published in the future
+        """
+
+        return Question.objects.filter(
+                pub_date__lte=timezone.now()
+            ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
