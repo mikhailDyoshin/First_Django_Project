02433d79f9f56b51ba7d1ea822403a000bc2a8bc
Mikhail Dyoshin <mikhail.dyoshin@gmail.com>
2022-10-10 10:11:54 +0300

    Customized the admin change list
    Now question fields are displayed in columns, there is a filter by date published and search box that searches the question_text field.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -16,5 +16,14 @@ class QuestionAdmin(admin.ModelAdmin):
 
     inlines = [ChoiceInLIne]
 
+    list_display = (
+        'question_text',
+        'pub_date', 
+        'was_published_recently'
+    )
+
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
     
 admin.site.register(Question, QuestionAdmin)
